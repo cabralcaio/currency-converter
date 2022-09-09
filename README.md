bc2fad2a27d47d17b1737320ab149826dcb2a226
CaioC <caio_cabral1@hotmail.com>
2022-09-09 15:24:33 -0300

    Second Commit

---
+++ README.md
@@ -0,0 +1,20 @@
+# App Web Currency Converter 💱
+
+Esse app é desenvolvido em linguagem Javascript, HTML e CSS. A principal função é mostrar a **cotação** em tempo real da conversão de uma moeda (Exemplo, cotação do Dólar em Real). Inicialmente é possível saber o câmbio entre 5 moedas, são elas:
+
+ - Dólar (USD)
+ - Real (BRL)
+ - Libra Esterlina (GBP)
+ - Euro (EUR)
+
+This app is developed using JavaScript, HTML and CSS. The main function is to show the exnchange rate in real time (Example, Dollar price in BRL). Initially it's possible to know the exchange between 5 currencies, they are:
+
+ - Dollar (USD)
+ - Real (BRL)
+ - Sterling Pound (GBP)
+ - Euro (EUR)
+
+## Demo
+<p align="center">
+    <img src="src/monetario.gif">
+</p>
--- scripts.js
+++ scripts.js
@@ -4,7 +4,7 @@ const btnInvert = document.querySelector("#invert");
 btn.addEventListener("click", function(e) {
     e.preventDefault();
     
-    let valor = parseInt(document.querySelector("#valor").value);
+    let valor = parseFloat(document.querySelector("#valor").value);
     const estate1 = document.querySelector("#estate1").value;
     const estate2 = document.querySelector("#estate2").value;
     const BASE_URL = `https://free.currconv.com/api/v7/convert?q=${estate1}_${estate2}&compact=ultra&apiKey=2df8c70666762f6b85c2`
@@ -18,7 +18,7 @@ btn.addEventListener("click", function(e) {
                 .then(response =>  response.json())
                 .then(resultado => {
                     const resultado1 = resultado[`${estate1}_${estate2}`] * valor
-                    document.getElementById("p1").innerHTML = `${valor} ${estate1} = ${resultado1} ${estate2}`
+                    document.getElementById("p1").innerHTML = `${valor} ${estate1} = ${resultado1.toFixed(2)} ${estate2}`
                 })
         }
         thenConverter();
